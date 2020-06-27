0d619cb220261e168599b02f1a25dc961e302a5b
Jools Wills <buzz@exotica.org.uk>
2020-06-28 00:11:52 +0100

    lr-beetle-psx - enabled aarch64 dynarec and configuration options suitable for non x64  * reworked logic for different targets

--- scriptmodules/libretrocores/lr-beetle-psx.sh
+++ scriptmodules/libretrocores/lr-beetle-psx.sh
@@ -17,7 +17,8 @@ rp_module_section="opt x86=main"
 rp_module_flags="!arm"
 
 function depends_lr-beetle-psx() {
-    local depends=(libvulkan-dev libgl1-mesa-dev)
+    local depends=()
+    isPlatform "gl" && depends+=(libvulkan-dev libgl1-mesa-dev)
     getDepends "${depends[@]}"
 }
 
@@ -26,23 +27,37 @@ function sources_lr-beetle-psx() {
 }
 
 function build_lr-beetle-psx() {
+    local params=()
+    local target="mednafen_psx_libretro.so"
+    if isPlatform "gl"; then
+        params+=(HAVE_HW=1)
+        target="mednafen_psx_hw_libretro.so"
+    fi
     make clean
-    make HAVE_HW=1
-    md_ret_require=(
-        'mednafen_psx_hw_libretro.so'
-    )
+    make "${params[@]}"
+    md_ret_require="$md_build/$target"
 }
 
 function install_lr-beetle-psx() {
-    md_ret_files=(
-        'mednafen_psx_hw_libretro.so'
-    )
+    local target="mednafen_psx_libretro.so"
+    isPlatform "x86" && target="mednafen_psx_hw_libretro.so"
+    md_ret_files="$target"
 }
 
 function configure_lr-beetle-psx() {
+    local target="mednafen_psx_libretro.so"
+    isPlatform "gl" && target="mednafen_psx_hw_libretro.so"
+
     mkRomDir "psx"
     ensureSystemretroconfig "psx"
 
-    addEmulator 0 "$md_id" "psx" "$md_inst/mednafen_psx_hw_libretro.so"
+    if ! isPlatform "gl"; then
+        # Use asyncronous CD loading on (typically low I/O) non-x86 platforms
+        setRetroArchCoreOption "beetle_psx_cd_access_method" "async"
+        # enable the dynamic recompiler for non-x86 platforms
+        setRetroArchCoreOption "beetle_psx_cpu_dynarec" "execute"
+    fi
+
+    addEmulator 0 "$md_id" "psx" "$md_inst/$target"
     addSystem "psx"
 }
